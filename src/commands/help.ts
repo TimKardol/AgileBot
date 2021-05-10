08e4a60a8ba5afe9256e71e92074471adda88fb8
TimKardol <tnmkardol@outlook.com>
2021-05-10 21:05:40 +0200

    Finished help command

---
+++ src/commands/help.ts
@@ -0,0 +1,28 @@
+import { Message, MessageEmbed } from "discord.js";
+
+module.exports = {
+    name: 'help',
+    description: 'Shows available commands',
+    args: false,
+    aliases: [],
+    usage: 'Help',
+    guildOnly: true,
+    cooldown: 3,
+    execute(message: Message, args: string[]) {
+        const embeddedMessage = new MessageEmbed;
+        embeddedMessage
+            .setColor('#3498db')
+            .setAuthor('HZ - University of Applied Sciences', 'https://vmh-hbo.nl/wp-content/uploads/2018/06/hz-logo.jpg')
+            .addFields(
+                { name: 'Available commands', value:`
+                !teamcoach | !tc
+                !tracker | !tk
+                !onsitecustomer | !osc
+                !developer | !dev
+                `}
+
+            )
+            .setTimestamp()
+        message.channel.send(embeddedMessage);
+    },
+};
